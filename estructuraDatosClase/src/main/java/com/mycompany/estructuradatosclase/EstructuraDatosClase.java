2f2d5dbadace16a74851af2c8693c311b4b14023
sebastian gomez <sebastian.gomez@peoplesvoice.co>
2022-06-09 15:15:58 -0500

    Relizacion del primer ejercicio arreglos 1

--- estructuraDatosClase/src/main/java/com/mycompany/estructuradatosclase/EstructuraDatosClase.java
+++ estructuraDatosClase/src/main/java/com/mycompany/estructuradatosclase/EstructuraDatosClase.java
@@ -5,6 +5,8 @@
 
 package com.mycompany.estructuradatosclase;
 
+import javax.swing.JOptionPane;
+
 /**
  *
  * @author sese4
@@ -12,6 +14,27 @@ package com.mycompany.estructuradatosclase;
 public class EstructuraDatosClase {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+ 
+        //Esto es opcional
+        final int CANTIDAD=5;
+        int num[]=new int[CANTIDAD];
+ 
+        //Invocamos las funciones
+        index(num);
+ 
+        showIndex(num);
+    }
+        //Ventana que solicita el numero
+    public static void index(int lista[]){
+        for(int i=0;i<lista.length;i++){
+            String texto=JOptionPane.showInputDialog("Ingresa el numero");
+            lista[i]=Integer.parseInt(texto);
+        }
+    }
+        //Se muestra el index con el numero respectivo indicado por el usuario o cliente
+    public static void showIndex(int lista[]){
+        for(int i=0;i<lista.length;i++){
+            System.out.println("Index "+i+" tiene el siguiente valor "+lista[i]);
+        }
     }
 }
