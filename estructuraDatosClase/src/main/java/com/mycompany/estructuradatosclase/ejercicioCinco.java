f2c6d9f762302c55cbab9b5256ab01d4a6004d03
sebastian gomez <sebastian.gomez@peoplesvoice.co>
2022-06-10 11:14:46 -0500

    ajustes

---
+++ estructuraDatosClase/src/main/java/com/mycompany/estructuradatosclase/ejercicioCinco.java
@@ -0,0 +1,44 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.estructuradatosclase;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author sese4
+ */
+public class ejercicioCinco {
+    public static void main(String[] args) {
+ 
+        Scanner sc = new Scanner(System.in);
+ 
+        char mayusculas[] = new char[26];
+        for (int i = 65, j = 0; i <= 90; i++, j++) {
+            mayusculas[j] = (char) i;
+        }
+ 
+        String cadena = "";
+        int eleccion = -1;
+ 
+        do {
+            System.out.println("Elija un indice entre 0 y " + (mayusculas.length - 1));
+            eleccion = sc.nextInt();
+ 
+            if (!(eleccion >= 0 && eleccion <= mayusculas.length - 1)) {
+                System.out.println("Error, inserte otro numero");
+            } else {
+                if (eleccion != -1) {
+                    cadena += mayusculas[eleccion];
+                }
+            }
+ 
+        } while (eleccion != -1);
+ 
+        System.out.println(cadena);
+ 
+    }
+    
+}
