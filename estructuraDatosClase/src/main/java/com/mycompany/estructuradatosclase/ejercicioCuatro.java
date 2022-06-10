0c0e13c0e6aaa137d5c6f426c4878630ccf18c57
sebastian gomez <sebastian.gomez@peoplesvoice.co>
2022-06-10 11:13:41 -0500

    Ejercicio 4

---
+++ estructuraDatosClase/src/main/java/com/mycompany/estructuradatosclase/ejercicioCuatro.java
@@ -0,0 +1,37 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.estructuradatosclase;
+
+/**
+ *
+ * @author sese4
+ */
+public class ejercicioCuatro {
+    public static void main(String[] args) {
+         
+        //Creo un array
+        int[] numeros=new int[100];
+         
+        //Declaro las variables necesarias
+        int suma=0;
+        double media;
+         
+        //Recorro el array, asigno números y sumo
+        for(int i=0;i<numeros.length;i++){
+            numeros[i]=i+1;
+            suma+=numeros[i];
+        }
+         
+        //Calculo la media y muestro la suma y la meda
+        System.out.println("La suma es "+suma);
+         
+        media=(double)suma/numeros.length;
+         
+        System.out.println("La media es "+media);
+         
+    }
+     
+    
+}
